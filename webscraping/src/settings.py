2af9fa6ee0937cca3183657478052e1f6bc5d741
Viktor Mihalyi <viktor.mihalyi@gmail.com>
2018-10-30 21:10:57 +0100

    elastic url

--- webscraping/src/settings.py
+++ webscraping/src/settings.py
@@ -1,6 +1,6 @@
 BOT_NAME = 'temalab'
 
-ELASTICSEARCH_URL = 'http://localhost:9200'
+ELASTICSEARCH_URL = 'http://elasticsearch:9200'
 
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
 
