0d8be282e11c461d303445d54bd7d176a6a4c23a
Viktor Mihalyi <viktor.mihalyi@gmail.com>
2018-12-13 15:44:25 +0100

    small fixes

--- webscraping/src/settings.py
+++ webscraping/src/settings.py
@@ -4,7 +4,7 @@ ELASTICSEARCH_URL = 'http://elasticsearch:9200'
 
 USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/70.0.3538.77 Chrome/70.0.3538.77 Safari/537.36'
 
-ROBOTSTXT_OBEY = False
+ROBOTSTXT_OBEY = True
 
 DOWNLOAD_DELAY = 5
 
